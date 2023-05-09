ae2eb95671d9a27e206279807ad09e202ee70d33
Aaron He <aaronhe09@yahoo.com>
2023-05-09 20:54:08 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-loss-of-precision */
+const orderHistory = [
+  {
+    orderDetail: {
+      orderNumber: 11439416898772232,
+      orderPlaced: 'August 4, 2020',
+      orderDelivered: 'August 8, 2020',
+      numberOfItems: 1,
+      total: 34.0,
+      shipTo: 'JS Masher',
+    },
+    itemsDetail: [
+      {
+        name: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr.Axel',
+        total: 34.0,
+      },
+    ],
+  },
+  {
+    orderDetail: {
+      orderNumber: 11399842681280257,
+      orderPlaced: 'July 19, 2020',
+      orderDelivered: 'July 20, 2020',
+      numberOfItems: 1,
+      total: 44.53,
+      shipTo: 'JS Masher',
+    },
+    itemsDetail: [
+      {
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        total: 44.53,
+      },
+    ],
+  },
+  {
+    orderDetail: {
+      orderNumber: 11428755579059409,
+      orderPlaced: 'July 4, 2020',
+      orderDelivered: 'July 7, 2020',
+      numberOfItems: 1,
+      total: 17.22,
+      shipTo: 'JS Masher',
+    },
+    itemsDetail: [
+      {
+        name: 'Gamecube Controller Adapter',
+        author: null,
+        total: 17.22,
+      },
+    ],
+  },
+  {
+    orderDetail: {
+      orderNumber: 11328831772648248,
+      orderPlaced: 'July 3, 2020',
+      orderDelivered: 'July 5, 2020',
+      numberOfItems: 2,
+      total: 138.93,
+      shipTo: 'JS Masher',
+    },
+    itemsDetail: [
+      {
+        name: 'GameCube Controller',
+        author: null,
+        total: 94.45,
+      },
+      {
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        total: 33.99,
+      },
+    ],
+  },
+];
+
+console.log(orderHistory[0].itemsDetail[0].total);
