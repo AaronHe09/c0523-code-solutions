6c15337f578131d41bebd5dcfdde1789814f5285
Aaron He <aaronhe09@yahoo.com>
2023-05-25 18:22:06 +0000

    completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,11 @@
+const taskList = document.querySelector('.task-list');
+
+taskList.addEventListener('click', function (e) {
+  const closest = e.target.closest('.task-list-item');
+
+  console.log('Event Target: ', e.target);
+  console.log('Event Tag Name: ', e.target.tagName);
+  console.log('Closest: ', closest);
+
+  closest.remove();
+});
