6fc15b04a247aa0e441d7950fb815577e0982e95
Aaron He <aaronhe09@yahoo.com>
2023-07-20 23:22:55 +0000

    added seperate error handling for auth

--- api-authorization/server/lib/authorization-middleware.js
+++ api-authorization/server/lib/authorization-middleware.js
@@ -5,8 +5,9 @@ import { ClientError } from './client-error.js';
 export function authMiddleware(req, res, next) {
   /* your code here */
   const auth = req.get('Authorization');
+  if (!auth) throw new ClientError(401, 'authentication required');
   const token = auth.split('Bearer ')[1];
-  if (!token || !auth) throw new ClientError(401, 'authentication required');
+  if (!token) throw new ClientError(401, 'authentication required');
   const payload = jwt.verify(token, process.env.TOKEN_SECRET);
   req.user = payload;
   next();
